504ed6c14b56e55ccd9bb400f39220fd5383c46d
maumruiz <mau.mruiz@gmail.com>
2025-02-08 13:03:39 -0600

    Create simple form to test messages

---
+++ webapp/src/components/SimpleInput.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  message: z.string().min(2).max(50),
+});
+
+export default function SimpleInput() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsSubmitting(true);
+    // Simulate an API call
+    setTimeout(() => {
+      console.log(values);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      );
+      form.reset(); // This will clear the input
+      setIsSubmitting(false);
+    }, 1000); // Simulating a 1-second delay
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex w-full max-w-sm items-center space-x-2"
+      >
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem className="grow">
+              {/* <FormLabel>Message</FormLabel> */}
+              <FormControl>
+                <Input placeholder="message..." {...field} disabled={isSubmitting} />
+              </FormControl>
+              {/* <FormDescription>This is your public display name.</FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" disabled={isSubmitting}>
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+}
